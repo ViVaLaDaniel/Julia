fd8da71c1e0600a8c2c2e5a5c2be096bafe9ae5e
Daniel Zamyatin <wiwalafreedom@outlook.com>
2024-02-07 22:59:14 +0100

    Logo

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -15,8 +15,8 @@ import { BsTelephoneInboundFill } from "@kalimahapps/vue-icons";
 <template>
   <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white bg-opacity-80 p-3 backdrop-blur-sm">
     <a href="#">
-      <img src="../assets/KaitHendMade.svg" alt="Logo" class="h-9" />
-    </a>
+      <img src="../assets/Keit_Hend_Made.svg" alt="Logo" class="h-9" />
+     </a>
     <!-- Для мобильных устройств -->
     <div class="hidden md:flex items-center">
       <a href="tel:=48537617365" class="text-lg flex justify-center">+48537617365</a>
