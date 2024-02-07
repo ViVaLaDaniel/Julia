1a3c7bbeeab5b3f1cdde0a4727ff089857cfe398
Daniel Zamyatin <wiwalafreedom@outlook.com>
2024-02-07 15:24:53 +0100

    2

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -27,7 +27,7 @@
     </div>
   
     <!-- Карта -->
-    <div class="w-full md:w-3/5 h-full">
+    <div class="w-full md:w-1/2 ">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d152.18765116972773!2d16.87767691473513!3d52.38850647214209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4704457f4e305ad3%3A0x51c21bbeb6586d08!2sKeit.HandMade!5e0!3m2!1sru!2spl!4v1707312356598!5m2!1sru!2spl" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   </div>
